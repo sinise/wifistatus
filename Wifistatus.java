799d0386cac1e87d63e01ca15ad4fee91e408a55
sebastian <sebastian@momentos.dk>
2015-01-13 00:36:57 +0100

    lksajflsa

--- Wifistatus.java
+++ Wifistatus.java
@@ -1,6 +1,6 @@
 import java.io.*;
 //import java.sql.Timestamp;
-import java.util.Date;
+import java.util.*;
 import java.text.SimpleDateFormat;
 import java.net.*;
 public class Wifistatus
@@ -9,10 +9,10 @@ public class Wifistatus
     {
         while(true)
         {
-            
-//            boolean status = checkWifi();
-  //          logTofile(status, timestamp());
-checkWifi();
+
+            boolean status = checkWifi();
+            logTofile(status, timestamp());
+            System.out.println(status + timestamp());
                try{
                    Thread.sleep(2000);
                } catch(InterruptedException e) {
@@ -21,13 +21,13 @@ checkWifi();
         }
     }
 
-    public String timestamp()
+    public static String timestamp()
     {
         String timestamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
         return timestamp;
     }
 
-    public void logTofile(String status, String timestamp)
+    public static void logTofile(boolean status, String timestamp)
     {
         try {
             PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("wifilog.csv", true)));
@@ -37,16 +37,65 @@ checkWifi();
         } catch(Exception e){e.getMessage();}
     }
 
-    public static String checkWifi()
-    {
-        String ip = "not declared";
-        try{
-            System.out.println(NetworkInterface.getNetworkInterfaces());
-//InetAddress ip = InetAddress.getHostAddress();
-        } catch (Exception e) {
-             e.getMessage();
-          }
-        System.out.println(ip);
-        return ip;
+    public static boolean checkWifi() {
+        String response = "";
+        ProcessBuilder pb = new ProcessBuilder("/sbin/iwconfig", "wlan0");
+// | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'");
+//        ProcessBuilder pb = new ProcessBuilder("/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'");
+        pb.redirectErrorStream(true);
+        try {
+            Process shell = pb.start();
+            // To capture output from the shell
+            InputStream shellIn = shell.getInputStream();
+            // Wait for the shell to finish and get the return code
+            int shellExitStatus = shell.waitFor();
+            response = convertStreamToStr(shellIn);
+            shellIn.close();
+        }
+
+        catch (IOException e) {
+            System.out.println("Error occured while executing Linux command. Error Description: "
+            + e.getMessage());
+        }
+
+        catch (InterruptedException e) {
+            System.out.println("Error occured while executing Linux command. Error Description: "
+            + e.getMessage());
+        }
+        if(response.length() > 300){
+            return true;
+        }
+            else
+        {
+            return false;
+        }
+    }
+
+/*
+* To convert the InputStream to String we use the Reader.read(char[]
+* buffer) method. We iterate until the Reader return -1 which means
+* there's no more data to read. We use the StringWriter class to
+* produce the string.
+*/
+
+    public static String convertStreamToStr(InputStream is) throws IOException {
+       if (is != null) {
+           Writer writer = new StringWriter();
+           char[] buffer = new char[1024];
+           try {
+               Reader reader = new BufferedReader(new InputStreamReader(is,"UTF-8"));
+               int n;
+               while ((n = reader.read(buffer)) != -1) {
+                   writer.write(buffer, 0, n);
+               }
+           }
+           finally {
+               is.close();
+           }
+           return writer.toString();
+       }
+       else {
+           return "";
+       }
     }
 }
